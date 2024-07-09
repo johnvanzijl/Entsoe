1bdfecafd6732e71551a6474a37ae115ca8806a4
johnvanzijl <john@annieenjohn.nl>
2024-07-09 21:34:10 +0200

    Update __init__.py

--- custom_components/entsoeprices/__init__.py
+++ custom_components/entsoeprices/__init__.py
@@ -1,28 +1,16 @@
 """The ENTSO-E Prices integration."""
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.typing import ConfigType
 
-from .const import DOMAIN, PLATFORMS
-
-async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    """Set up the ENTSO-E Prices component."""
-    return True
+from .const import DOMAIN
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up ENTSO-E Prices from a config entry."""
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = entry.data
-
-    for component in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, component)
-        )
+    hass.config_entries.async_setup_platforms(entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload ENTSO-E Prices config entry."""
-    for component in PLATFORMS:
-        await hass.config_entries.async_forward_entry_unload(entry, component)
-    hass.data[DOMAIN].pop(entry.entry_id)
+    await hass.config_entries.async_unload_platforms(entry, ["sensor"])
     return True
