d04c0d0a7a70af0c5054724825a787a61cc9ad38
johnvanzijl <john@annieenjohn.nl>
2024-07-09 22:04:35 +0200

    Update __init__.py

--- custom_components/entsoeprices/__init__.py
+++ custom_components/entsoeprices/__init__.py
@@ -10,9 +10,17 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(entry, "sensor")
     )
+
+    async def handle_update(call):
+        """Handle the service call to update data."""
+        coordinator = hass.data[DOMAIN][entry.entry_id]
+        await coordinator.async_request_refresh()
+
+    hass.services.async_register(DOMAIN, "update", handle_update)
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload ENTSO-E Prices config entry."""
     await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    hass.services.async_remove(DOMAIN, "update")
     return True
