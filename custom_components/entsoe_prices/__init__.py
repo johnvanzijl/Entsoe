5643df3e0b25cd879ba535642c252d3e3867a4db
johnvanzijl <john@annieenjohn.nl>
2024-07-09 18:45:24 +0200

    Add files via upload

--- custom_components/entsoe_prices/__init__.py
+++ custom_components/entsoe_prices/__init__.py
@@ -1,28 +1,28 @@
 """The ENTSO-E Prices integration."""
-from datetime import timedelta
-import logging
-
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
 
 from .const import DOMAIN, PLATFORMS
 
-_LOGGER = logging.getLogger(__name__)
-
-async def async_setup(hass: HomeAssistant, config: dict):
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up the ENTSO-E Prices component."""
     return True
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up ENTSO-E Prices from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+
     for component in PLATFORMS:
         hass.async_create_task(
             hass.config_entries.async_forward_entry_setup(entry, component)
         )
     return True
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload ENTSO-E Prices config entry."""
     for component in PLATFORMS:
         await hass.config_entries.async_forward_entry_unload(entry, component)
+    hass.data[DOMAIN].pop(entry.entry_id)
     return True
